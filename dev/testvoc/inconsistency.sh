9e7617b7fb79d8b2c35b45c62fb147a938a985ea
Francis M. Tyers <ftyers@prompsit.com>
2014-07-16 14:00:38 +0000

    add testvoc

---
+++ dev/testvoc/inconsistency.sh
@@ -0,0 +1,30 @@
+TMPDIR=/tmp
+
+DICT=$1
+DIR=$2
+
+SED=sed
+
+if [[ $DIR = "urd-hin" ]]; then
+
+# Run the bilingual dictionary before to make sure we are only checking things we have.
+lt-expand $DICT | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | $SED 's/:>:/%/g' | $SED 's/:/%/g' | cut -f2 -d'%' |  $SED 's/^/^/g' | $SED 's/$/$ ^.<sent>$/g' | apertium-pretransfer | lt-proc -b ../../urd-hin.autobil.bin | grep -v '/@' | cut -f1 -d'/' | $SED 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |\
+        apertium-pretransfer|\
+	lt-proc -b ../../urd-hin.autobil.bin | tee $TMPDIR/$DIR.tmp_testvoc2.txt |\
+        apertium-transfer -b ../../apertium-urd-hin.urd-hin.t1x  ../../urd-hin.t1x.bin |\
+        lt-proc -d ../../urd-hin.autogen.bin > $TMPDIR/$DIR.tmp_testvoc3.txt
+paste -d _ $TMPDIR/$DIR.tmp_testvoc1.txt $TMPDIR/$DIR.tmp_testvoc2.txt $TMPDIR/$DIR.tmp_testvoc3.txt | $SED 's/\^.<sent>\$//g' | $SED 's/_/   --------->  /g'
+
+elif [[ $DIR = "hin-urd" ]]; then 
+
+# Run the bilingual dictionary before to make sure we are only checking things we have.
+lt-expand $DICT | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | $SED 's/:>:/%/g' | $SED 's/:/%/g' | cut -f2 -d'%' |  $SED 's/^/^/g' | $SED 's/$/$ ^.<sent>$/g' | apertium-pretransfer | lt-proc -b ../../hin-urd.autobil.bin | grep -v '/@' | cut -f1 -d'/' | $SED 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |\
+        apertium-pretransfer|\
+	lt-proc -b ../../hin-urd.autobil.bin | tee $TMPDIR/$DIR.tmp_testvoc2.txt |\
+        apertium-transfer -b ../../apertium-urd-hin.hin-urd.t1x  ../../hin-urd.t1x.bin |\
+        lt-proc -d ../../hin-urd.autogen.bin > $TMPDIR/$DIR.tmp_testvoc3.txt
+paste -d _ $TMPDIR/$DIR.tmp_testvoc1.txt $TMPDIR/$DIR.tmp_testvoc2.txt $TMPDIR/$DIR.tmp_testvoc3.txt | $SED 's/\^.<sent>\$//g' | $SED 's/_/   --------->  /g'
+
+else
+	echo "bash inconsistency.sh <direction>";
+fi
---
+++ dev/testvoc/testvoc.sh
@@ -0,0 +1,9 @@
+if [[ $2 = "urd-hin" ]]; then
+echo "==Urdu->Hindi===========================";
+bash inconsistency.sh $1 urd-hin > /tmp/urd-hin.testvoc; bash inconsistency-summary.sh /tmp/urd-hin.testvoc urd-hin
+echo ""
+elif [[ $2 = "hin-urd" ]]; then
+echo "==Hindi->Urdu===========================";
+bash inconsistency.sh $1 hin-urd > /tmp/hin-urd.testvoc; bash inconsistency-summary.sh /tmp/hin-urd.testvoc hin-urd
+echo "";
+fi
